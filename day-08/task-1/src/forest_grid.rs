8b3782423d153dd8e2df070f4932a130dd5fce0f
Ian Anderson <ian@gozumi.com>
2023-02-12 23:25:55 +0000

    Add day 8 answer - task 1. wip

--- day-08/task-1/src/forest_grid.rs
+++ day-08/task-1/src/forest_grid.rs
@@ -5,20 +5,22 @@ struct ForwardCounting {
 }
 
 
-#[derive(Debug)]
-struct BockwardCounting {
+#[derive(Clone, Debug)]
+struct BackwardCounting {
     visible_heights: Vec<u32>,
 }
 
 #[derive(Debug)]
 pub struct VisibilityCountStruct {
     from_left_collection: Vec<ForwardCounting>,
-    from_right_collection: Vec<BockwardCounting>,
+    from_right_collection: Vec<BackwardCounting>,
     from_top: Vec<ForwardCounting>,
-    // from_bottom: Vec<BockwardCounting>,
+    from_bottom: Vec<BackwardCounting>,
     total_visible_from_left: u32,
     total_visible_from_right: u32,
     total_visible_from_top: u32,
+    total_visible_from_bottom: u32,
+    visibility_grand_total: u32
 }
 
 #[derive(Debug)]
@@ -53,10 +55,12 @@ pub fn get_visibility_count(
                 from_left_collection: vec![],
                 from_right_collection: vec![],
                 from_top: vec![],
-                // from_bottom: vec![],
+                from_bottom: vec![],
                 total_visible_from_left: 0,
                 total_visible_from_right: 0,
                 total_visible_from_top: 0,
+                total_visible_from_bottom: 0,
+                visibility_grand_total: 0,
             };
             return VisibilityCount::Some(v_count);
         }
@@ -72,7 +76,7 @@ fn get_visibility_count_details(
         tallest_height: 0,
     };
 
-    let mut from_right = BockwardCounting {
+    let mut from_right = BackwardCounting {
         visible_heights: vec![],
     };
 
@@ -81,9 +85,13 @@ fn get_visibility_count_details(
         tallest_height: 0,
     };
 
+    let mut from_bottom = BackwardCounting {
+        visible_heights: vec![],
+    };
+
     for (i, tree_height) in grid_row.iter().enumerate() {
         from_left = get_forward_visibility_count_details(from_left, *tree_height);
-        from_right = get_right_visibility_count_details(from_right, *tree_height);
+        from_right = get_backward_visibility_count_details(from_right, *tree_height);
         from_top = match visibility_count.from_top.get(i) {
             Some(from_top) => {
                 let r = *from_top;
@@ -102,14 +110,41 @@ fn get_visibility_count_details(
         };
         from_top = get_forward_visibility_count_details(from_top, *tree_height);
         visibility_count.from_top[i] = from_top.clone();
+
+        from_bottom = match visibility_count.from_bottom.get(i) {
+            Some(from_bottom) => {
+                let mut r:BackwardCounting = BackwardCounting {
+                    visible_heights: vec![],
+                };
+                from_bottom.clone_into(&mut r);
+                r
+            },
+            None => {
+                from_bottom = BackwardCounting {
+                    visible_heights: vec![],
+                };
+
+                visibility_count.from_bottom.push(from_bottom);
+                let from_bottom = visibility_count.from_bottom.get(i).unwrap();
+                let mut r:BackwardCounting = BackwardCounting {
+                    visible_heights: vec![],
+                };
+                from_bottom.clone_into(&mut r);
+                r
+            },
+        };
+        from_bottom = get_backward_visibility_count_details(from_bottom, *tree_height);
+        visibility_count.from_bottom[i] = from_bottom.clone();
     }
 
     visibility_count.total_visible_from_left += from_left.visible_count;
     visibility_count.total_visible_from_right += from_right.visible_heights.len() as u32;
     visibility_count.total_visible_from_top += from_top.visible_count;
+    visibility_count.total_visible_from_bottom += from_bottom.visible_heights.len() as u32;
     visibility_count.from_left_collection.push(from_left);
     visibility_count.from_right_collection.push(from_right);
-    visibility_count.from_left_collection.push(from_top);
+
+    visibility_count.visibility_grand_total = visibility_count.total_visible_from_left + visibility_count.total_visible_from_right + visibility_count.total_visible_from_top + visibility_count.total_visible_from_bottom;
 
     return visibility_count;
 }
@@ -126,15 +161,15 @@ fn get_forward_visibility_count_details(
     return foward_counting;
 }
 
-fn get_right_visibility_count_details(
-    mut from_right: BockwardCounting,
+fn get_backward_visibility_count_details(
+    mut backward_counting: BackwardCounting,
     tree_height: u32,
-) -> BockwardCounting {
+) -> BackwardCounting {
     loop {
-        let last_heigth = match from_right.visible_heights.pop() {
+        let last_heigth = match backward_counting.visible_heights.pop() {
             Some(height) => height,
             None => {
-                from_right.visible_heights.push(tree_height);
+                backward_counting.visible_heights.push(tree_height);
                 break;
             }
         };
@@ -142,11 +177,11 @@ fn get_right_visibility_count_details(
         if last_heigth <= tree_height {
             continue;
         } else {
-            from_right.visible_heights.push(last_heigth);
-            from_right.visible_heights.push(tree_height);
+            backward_counting.visible_heights.push(last_heigth);
+            backward_counting.visible_heights.push(tree_height);
             break;
         }
     }
 
-    return from_right;
+    return backward_counting;
 }
